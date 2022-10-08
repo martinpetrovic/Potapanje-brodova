e0816a2f5ccc4206a93bb6ea42eb22a1bdd851d3
martinpetrovic <106666726+martinpetrovic@users.noreply.github.com>
2022-10-08 15:48:51 +0200

    Add files via upload

--- POCETNA VERZIJA POTAPANJE 7.10.2022 Martinov Loading Screen.py
+++ POCETNA VERZIJA POTAPANJE 7.10.2022 Martinov Loading Screen.py
@@ -4,7 +4,8 @@ import os
 import time
 pygame.init()
 pygame.mixer.init()
- 
+from pygame.locals import * 
+
 ŠIRINA, VISINA = 1280, 720
 PROZOR = pygame.display.set_mode((ŠIRINA, VISINA))
 pygame.display.set_caption("Potapanje brodova")
@@ -17,7 +18,7 @@ WHITE = (255,255,255)
 LOGO = pygame.image.load(os.path.join("potapanje brodova", "MLKJR_LOGO.png" ))
 INTRO = pygame.mixer.Sound(os.path.join("potapanje brodova", "INTRO.ogg"))
 
-
+KVADRAT = pygame.image.load(os.path.join("potapanje brodova", "kvadrat.png"))
 
 ## OVO MORA LUKA DOVRŠIT
 class Button:
@@ -40,7 +41,9 @@ class Button:
         mouse_poz = pygame.mouse.get_pos()
         if self.top_rect.collidepoint(mouse_poz):
             if pygame.mouse.get_pressed()[0]:
-                #if self.name == 'Play':
+                if self.name == 'Play':
+                    GRIDLIJEVO()
+                    GRIDDESNO()
                 if self.name == 'Exit game':
                     #Možemo dodat da iskoči prozor i pita 'Are you sure you want to exit the game?'
                     pygame.quit()
@@ -53,7 +56,33 @@ def LOADING_SCREEN():
     time.sleep(4)                
     PROZOR.fill(WHITE)       
               
+def GRIDLIJEVO():
+    PROZOR.fill(WHITE)
+    y = 100
+    x = 0
+    for i in range(10):
+        KVADRATRECT = KVADRAT.get_rect(topleft = (x,y))
+        y = y + 50
+        x = 0
+        for i in range (10):
+            x = x + 50
+            KVADRATRECT = KVADRAT.get_rect(topleft = (x,y))
+            PROZOR.blit(KVADRAT,KVADRATRECT)
+    pygame.display.update()
 
+def GRIDDESNO():
+    #PROZOR.fill(WHITE)
+    y = 100
+    x = 690
+    for i in range(10):
+        KVADRATRECT = KVADRAT.get_rect(topleft = (x,y))
+        y = y + 50
+        x = 690
+        for i in range (10):
+            x = x + 50
+            KVADRATRECT = KVADRAT.get_rect(topleft = (x,y))
+            PROZOR.blit(KVADRAT,KVADRATRECT)
+    pygame.display.update()
 
 def main():
     clock = pygame.time.Clock()
